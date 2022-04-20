ef28cfb3d19342e243dfb72e63a9dc9ce16c692e
RsTs23 <43611813+RsTs23@users.noreply.github.com>
2022-04-20 13:11:05 +0200

    Fixed certificate test
    Order of input matters since dart doesn't sort them automatically.

--- thor_devkit_dart/test/certificate_test.dart
+++ thor_devkit_dart/test/certificate_test.dart
@@ -9,21 +9,21 @@ import 'package:thor_devkit_dart/utils.dart';
 void main() {
   Uint8List priv = hexToBytes(
       "7582be841ca040aa940fff6c05773129e135623e41acce3e0b8ba520dc1ae26a");
-  String addr = Address.publicKeyToAddressString(derivePublicKeyFromBytes(priv, false));
+  String addr =
+      Address.publicKeyToAddressString(derivePublicKeyFromBytes(priv, false));
   String sig =
       "0x390870e4a99a6a80c3903e0bc13fdcaf15ae46d27b6365e3e07275990e3e74955ad43dba79682b9d0de3a47e96149539b07dde6b51c49a1c7eb6254036b913b000";
 
-
   test('verify correct', () {
     Map<String, String> m = <String, String>{};
-    m['type'] = 'text';
     m['content'] = 'fyi';
+    m['type'] = 'text';
 
     Certificate c = Certificate(
-        "identification", m, "localhost", 1545035330, addr, 
+        "identification", m, "localhost", 1545035330, addr,
         signature: sig);
- 
-      c.verify();
+
+    c.verify();
   });
 
   test('not a valid sig', () {
@@ -97,7 +97,4 @@ cert2 = certificate.Certificate(**cert2_dict)
 
 
 */
-
 }
-
-
